8720fa9bffd119ef13918891407cebda534689d8
GloriaRoger <roger.gloria77@gmail.com>
2023-04-30 16:16:38 -0400

    1st Commit

---
+++ timersexcersize.js
@@ -0,0 +1,27 @@
+
+function countDown(num) {
+    let timer = setInterval(function() {
+        num--;
+        if(num <= 0) {
+            clearInterval(timer);
+            console.log('DONE');
+        }
+        else {
+            console.log(num);
+        }
+    } , 1000)
+}
+
+
+function randomGame() {
+    let num;
+    let counter = 0;
+    let int = setInterval(function() {
+        num = Math.random();
+        counter++;
+        if (num > .75) {
+            clearInterval(int);
+            console.log("It Took" + counter + " try/tries");
+        }
+    } , 1000)
+}
