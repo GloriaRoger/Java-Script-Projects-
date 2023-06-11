105ad69e5d33325a8f8d39e435f2d3d18e4f4787
GloriaRoger <roger.gloria77@gmail.com>
2023-06-11 19:01:47 -0400

    eventListener

--- event.js
+++ event.js
@@ -1,4 +1,26 @@
-function makeBodyPurple() {
-    document.body.style.backgroundColor = 'purple';
-    }
-    const btn = document.querySelector('#teal');
+//Creating the Function "Make Body" in order to callit in html for button "Click Me" in the body. 
+function makeBody(color) {
+    document.body.style.backgroundColor = color;
+}
+
+//Creating a variable that then can call a funtion by using var.onsomeaction = to the function to makeBody('color')
+const btn = document.querySelector('#teal');
+btn.onclick = function() {
+    makeBody('teal');
+}; 
+const h1 = document.querySelector('h1');
+btn.onclick = function() {
+    h1.style.color = 'cyan';
+};
+
+//USE eventListener over inLine and setting events manually because they don't allow mfor multiple events to the same element whereas eventLister does
+//adding event listener to an id. using variable violetbtn we are able to call the built in JS function addEventListener to hold ("event,  function () { make body ('color');)
+const voiletBtn = document.querySelector('#violet');
+
+voiletBtn.addEventListener('click', function() {
+    makeBody('Violet');
+});
+
+voiletBtn.addEventListener('click', function() {
+    h1.style.color = 'cyan';
+});
