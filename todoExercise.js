8281b64f89176cb38f2a6c9830485c64e9131c63
GloriaRoger <roger.gloria77@gmail.com>
2023-06-25 19:27:32 -0400

    To Do List Exercise

---
+++ todoExercise.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let toDo = document.getElementById("toDo");
+    let toDoList = document.getElementById("toDoList");
+  
+    toDo.addEventListener("submit", function(e) {
+      e.preventDefault();
+  
+      let removeButton = document.createElement("button");
+      removeButton.innerText = "Remove";
+  
+      let addToDo = document.createElement("li");
+      addToDo.innerText = document.getElementById("addToDo").value;
+  
+      toDoList.appendChild(addToDo);
+      addToDo.appendChild(removeButton);
+  
+      toDo.reset();
+    });
+  
+    // a bad attemp at the lower solution
+
+//     toDoList.addEventListener("click", function(e) {
+//         if (addToDo === "li") {
+//         e.target.style.textDecoration = "line-through";
+//       } else if (targetTagToLowerCase === "button") {
+//         e.target.parentNode.remove();
+//       }
+//     });
+//   });
+
+    toDoList.addEventListener("click", function(event) {
+      const targetTagToLowerCase = event.target.tagName.toLowerCase();
+      if (targetTagToLowerCase === "li") {
+        event.target.style.textDecoration = "line-through";
+      } else if (targetTagToLowerCase === "button") {
+        event.target.parentNode.remove();
+      }
+    });
+  });
+
+  
+    
