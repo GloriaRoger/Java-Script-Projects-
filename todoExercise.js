35cace7d6a9fed1dd31dad47c531eac4917dde92
GloriaRoger <roger.gloria77@gmail.com>
2023-06-25 20:09:29 -0400

    To DO

--- todoExercise.js
+++ todoExercise.js
@@ -7,36 +7,44 @@ document.addEventListener("DOMContentLoaded", function() {
   
       let removeButton = document.createElement("button");
       removeButton.innerText = "Remove";
-  
+
       let addToDo = document.createElement("li");
       addToDo.innerText = document.getElementById("addToDo").value;
-  
+
       toDoList.appendChild(addToDo);
       addToDo.appendChild(removeButton);
   
       toDo.reset();
     });
-  
-    // a bad attemp at the lower solution
 
-//     toDoList.addEventListener("click", function(e) {
-//         if (addToDo === "li") {
-//         e.target.style.textDecoration = "line-through";
-//       } else if (targetTagToLowerCase === "button") {
-//         e.target.parentNode.remove();
-//       }
-//     });
-//   });
+    // removeButton.addEventListener("click", function(e) {
+    //   e.target.parentNode.remove();
+    // });
 
-    toDoList.addEventListener("click", function(event) {
-      const targetTagToLowerCase = event.target.tagName.toLowerCase();
-      if (targetTagToLowerCase === "li") {
-        event.target.style.textDecoration = "line-through";
-      } else if (targetTagToLowerCase === "button") {
-        event.target.parentNode.remove();
-      }
-    });
-  });
-
+    // addToDo.addEventListener("click", function(e) {
+    //   e.target.style.textDecoration = "line-through";
+    // });
   
     
+     
+      // a bad attemp at the lower solution
+  
+  //     toDoList.addEventListener("click", function(e) {
+  //         if (addToDo === "li") {
+  //         e.target.style.textDecoration = "line-through";
+  //       } else if (targetTagToLowerCase === "button") {
+  //         e.target.parentNode.remove();
+  //       }
+  //     });
+  //   });
+  
+      toDoList.addEventListener("click", function(event) {
+        const targetTagToLowerCase = event.target.tagName.toLowerCase();
+        if (targetTagToLowerCase === "li") {
+          event.target.style.textDecoration = "line-through";
+        } else if (targetTagToLowerCase === "button") {
+          event.target.parentNode.remove();
+        }
+      });
+    });
+  
